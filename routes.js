e1c8fece11e7754905494c07086c297a88a369a1
Ndagi Stanley <ndagis@gmail.com>
2021-03-18 07:27:29 +0300

    Initial commit

---
+++ routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const todos = require("./todos");
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  res.send("Welcome to the 'todos' API");
+});
+
+router.get("/todos", function(req, res) {
+  res.json(todos);
+});
+
+module.exports = router;
---
+++ todos.js
@@ -0,0 +1,15 @@
+
+module.exports = [
+  {
+    id: 1,
+    task: "Fix Sink"
+  },
+  {
+    id: 2,
+    task: "Buy Groceries"
+  },
+  {
+    id: 3,
+    task: "Wash the dishes"
+  }
+];
